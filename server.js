99a5a347bb4a0f7b6efcaf60519264cad11f65d3
minh <minhnha@smartosc.com>
2020-04-28 15:37:06 +0700

    upp

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/Project_SEM3_Admin'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/Project_SEM3_Admin/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
